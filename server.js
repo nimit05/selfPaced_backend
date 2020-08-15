5a3fc6650d1d2b1ad178bc85c0a59fcad534318d
Tushar Bhardwaj <iamtushar324@gmail.com>
2020-08-15 23:08:04 +0530

    pro
    Signed-off-by: Tushar Bhardwaj <iamtushar324@gmail.com>

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require("express-session");
 const path = require("path");
 const { auth } = require("./middleware/auth");
 
-// Development
+// Production
 // middlewares
 app.use(
   session({
